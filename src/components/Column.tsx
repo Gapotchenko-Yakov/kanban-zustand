325514c1992f7790dc2fecc51cfa5a5dade37b07
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-17 20:25:11 +0300

    Add Task component.

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 import "./Column.css";
+import Task from "./Task";
 
 const Column = ({ state }) => {
-  return <div className="column">{state}</div>;
+  return (
+    <div className="column">
+      <p>{state}</p>
+      <Task title="Todo" />
+    </div>
+  );
 };
 
 export default Column;
---
+++ src/components/Task.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Task.css";
+
+const Task = ({ title }) => {
+  return <div className="task">{title}</div>;
+};
+
+export default Task;
