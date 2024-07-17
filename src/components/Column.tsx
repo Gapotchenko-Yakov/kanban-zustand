217205a632958c3b2a48371b5884d81bb36fda37
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-17 20:55:31 +0300

    Use shallow fo performance

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 import "./Column.css";
 import Task from "./Task";
+import { useStore } from "../store";
+import { shallow } from "zustand/shallow";
 
 const Column = ({ state }) => {
+  const tasks = useStore(
+    (store) => store.tasks.filter((task) => task.state === state),
+    shallow
+  );
+
   return (
     <div className="column">
       <p>{state}</p>
