538d2f1ebb5d198e0e6e420086320494c77e6346
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-17 20:34:00 +0300

    Finished Task component.

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 
 import "./Task.css";
+import classNames from "classnames";
+
+const STATUS = "PLANNED";
 
 const Task = ({ title }) => {
-  return <div className="task">{title}</div>;
+  return (
+    <div className="task">
+      <div>{title}</div>
+      <div className="bottomWrapper">
+        <div></div>
+        <div className={classNames("status", STATUS)}>{STATUS}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Task;
