5b7bd7efcaa42d0284c7d1688283e7419864f427
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-17 22:26:15 +0300

    With modal for task adding.

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 
 import "./Column.css";
 import Task from "./Task";
@@ -6,6 +6,9 @@ import { useStore } from "../store";
 import { shallow } from "zustand/shallow";
 
 const Column = ({ state }) => {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
   const tasks = useStore(
     (store) => store.tasks.filter((task) => task.state === state),
     shallow
@@ -16,11 +19,27 @@ const Column = ({ state }) => {
     <div className="column">
       <div className="titleWrapper">
         <p>{state}</p>
-        <button onClick={() => addTask("asd" + state, state)}>Add</button>
+        <button onClick={() => setOpen(true)}>Add</button>
       </div>
       {tasks.map((task) => (
         <Task title={task.title} />
       ))}
+      {open && (
+        <div className="Modal">
+          <div className="modalContent">
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
